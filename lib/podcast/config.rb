bc931f3a61d7c0e1eda56d08f7c51d247a417121
Kenton Newby <kentonnewby@gmail.com>
2013-02-14 19:27:43 -0500

    added Config class   * got initial config file setup   * can initialize config from a block

--- lib/podcast/config.rb
+++ lib/podcast/config.rb
@@ -1,11 +1,21 @@
 module PodcastRB
   class Config
 
-    attr_accessor :source_dir
+    attr_accessor :source_dir, :destination_dir, :destination_file
     
     def initialize(args = {})
-      @source_dir = args.fetch(:source_dir)
-      
+      if block_given?
+        yield self
+      else
+        raise if args.fetch(:source_dir)       == nil
+        raise if args.fetch(:destination_dir)  == nil
+        raise if args.fetch(:destination_file) == nil
+
+        @source_dir       = args.fetch(:source_dir)
+        @destination_dir  = args.fetch(:destination_dir)
+        @destination_file = args.fetch(:destination_file)
+      end
+
     end
 
   end
--- spec/lib/podcast/config_spec.rb
+++ spec/lib/podcast/config_spec.rb
@@ -1,16 +1,92 @@
 require_relative "../../../lib/podcast/config"
+require 'pry'
+
+def default_args
+  {
+    :source_dir       => "/some/path/to/mp3_files",
+    :destination_dir  => "/some/destination/path/",
+    :destination_file => "test_feed.rss"
+  }
+end
 
 describe PodcastRB::Config do
-  describe "a valid config file" do
-    it "allows a source_dir to be specified" do
-      config = PodcastRB::Config.new(:source_dir => "/some/path/to/mp3_files")
+  let(:config) { PodcastRB::Config.new(default_args) }
 
+  describe "when required arguments are provided" do
+    it "source_dir is set" do
       config.source_dir.should == "/some/path/to/mp3_files"
     end
 
-    it "raises an exception if no source_dir is specified)" do
-      expect { config = PodcastRB::Config.new }.to raise_error KeyError
+    it "destination_dir is set" do
+      config.destination_dir.should == "/some/destination/path/"
     end
 
+    it "destination_file is set" do
+      config.destination_file.should == "test_feed.rss"
+    end
   end
+
+  describe "when required arguments are missing" do
+    it "raises an exception if no source_dir is specified" do
+      invalid_args = default_args
+      invalid_args.delete(:source_dir)
+      expect { config = PodcastRB::Config.new(invalid_args) }.to raise_error KeyError
+    end
+
+    it "raises an exception if no destination_dir is specified" do
+      invalid_args = default_args
+      invalid_args.delete(:destination_dir)
+      expect { config = PodcastRB::Config.new(invalid_args) }.to raise_error KeyError
+    end
+
+    it "raises an exception if no destination_file is specified" do
+      invalid_args = default_args
+      invalid_args.delete(:destination_file)
+      expect { config = PodcastRB::Config.new(invalid_args) }.to raise_error KeyError
+    end
+  end
+
+  describe "when required arguments are nil" do
+    it "raises an exception if source_dir is nil" do
+      invalid_args = default_args.merge(:source_dir => nil)
+      expect { config = PodcastRB::Config.new(invalid_args) }.to raise_error
+    end
+
+    it "raises an exception if destination_dir is nil"do 
+      invalid_args = default_args.merge(:destination_dir => nil)
+      expect { config = PodcastRB::Config.new(invalid_args) }.to raise_error
+    end
+
+    it "raises an exception if destination_file is nil" do 
+      invalid_args = default_args.merge(:destination_file => nil)
+      expect { config = PodcastRB::Config.new(invalid_args) }.to raise_error
+    end
+  end
+
+  describe "when a block is passed to initializer" do
+    it "sets source_dir within the block" do
+      config = PodcastRB::Config.new do |c|
+        c.source_dir =  "/some/path/to/mp3_files"
+      end
+
+      config.source_dir.should == "/some/path/to/mp3_files" 
+    end
+
+    it "sets destination_dir within the block" do
+      config = PodcastRB::Config.new do |c|
+        c.destination_dir =  "/some/destination/path/"
+      end
+
+      config.destination_dir.should ==  "/some/destination/path/"
+    end
+
+    it "sets destination_file within the block" do
+      config = PodcastRB::Config.new do |c|
+        c.destination_file =  "test_feed.rss"
+      end
+
+      config.destination_file.should ==  "test_feed.rss"
+    end
+  end
+
 end
