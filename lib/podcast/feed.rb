50f905e7c64ce47a255e1b22d50a73ef013e2a11
Kenton Newby <kentonnewby@gmail.com>
2013-07-30 09:25:29 -0400

    fixes tabs/spacing in feed.rb

--- lib/podcast/feed.rb
+++ lib/podcast/feed.rb
@@ -2,41 +2,41 @@ require_relative './config'
 
 module PodcastRB
 
-	class Feed
+  class Feed
 
-		DEFAULTS = {
-									:source_dir       => "/mp3s",
-									:destination_dir  => ".",
-									:destination_file => "podcast.rss"
-								}
+    DEFAULTS = {
+      :source_dir       => "/mp3s",
+      :destination_dir  => ".",
+      :destination_file => "podcast.rss"
+    }
 
-		attr_accessor :config, 
-									:title,
-									:domain,
-									:url,
-									:description,
-									:image,
-									:language,
-									:version,
-									:about,
-									:files
+    attr_accessor :config,
+                  :title,
+                  :domain,
+                  :url,
+                  :description,
+                  :image,
+                  :language,
+                  :version,
+                  :about,
+                  :files
 
-		def initialize(args = {})
-			@config      = PodcastRB::Config.new(DEFAULTS)
-			@title       = args.fetch(:title)       { "" }
-			@domain      = args.fetch(:domain)      { "" }
-			@url         = args.fetch(:url)         { "" }
-			@description = args.fetch(:description) { "" }
-			@image       = args.fetch(:image)       { "" }
-			@language    = args.fetch(:language)    { "" }
-			@version     = args.fetch(:version)     { "" }
-			@about       = args.fetch(:about)       { "" }
-			@files       = get_files
-		end
+    def initialize(args = {})
+      @config      = PodcastRB::Config.new(DEFAULTS)
+      @title       = args.fetch(:title)       { "" }
+      @domain      = args.fetch(:domain)      { "" }
+      @url         = args.fetch(:url)         { "" }
+      @description = args.fetch(:description) { "" }
+      @image       = args.fetch(:image)       { "" }
+      @language    = args.fetch(:language)    { "" }
+      @version     = args.fetch(:version)     { "" }
+      @about       = args.fetch(:about)       { "" }
+      @files       = get_files
+    end
 
-		def get_files
+    def get_files
 
-		end
+    end
 
-	end
+  end
 end
